2473117b5da21d4267140c170d973e3b0b6761dc
Acaruso <anthonycaruso102@gmail.com>
2024-07-09 07:04:42 -0400

    add lot of strings

--- src/ui_elts/FretboardElt.ts
+++ src/ui_elts/FretboardElt.ts
@@ -35,7 +35,7 @@ class FretboardElt extends BaseElt {
         // this.numCols = numCols;
 
         // this.numRows = 6;
-        this.numRows = 4;
+        this.numRows = 16;
         this.numCols = 30;
 
         this.rect = {
